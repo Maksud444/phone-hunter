8e6982e02027018d9c7f080dc9c50ae45fdf28ff
Maksud444 <83550908+Maksud444@users.noreply.github.com>
2022-08-30 18:12:30 +0600

    adede phone storage

--- phone.js
+++ phone.js
@@ -34,8 +34,7 @@ const displayPhones = (phones,dataLimit)=>{
           <div class="card-body">
             <h5 class="card-title">${phone.phone_name}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary">Show Details</button>
-
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary"data-bs-toggle="modal" data-bs-target="#phoneModalLavel">Show Details</button>
           </div>
         </div>
      `;
@@ -84,15 +83,17 @@ const loadPhoneDetails = async id =>{
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhoneDetails(data);
+    displayPhoneDetails(data.data);
 }
 
 const displayPhoneDetails = phone =>{
+    console.log(phone);
  const modalTitle = document.getElementById('phoneDetailModal');
  modalTitle.innerText = phone.name;
- const phoneDetails = document.getElementById('phone-detail');
+ const phoneDetails = document.getElementById('phone-details');
  phoneDetails.innerHTML = `
- <p>Release Data: ${phone}</p>
+ <p>Release Data: ${phone.releaseDate ? phone.releaseDate : 'No Release Date'}</p>
+ <p>Storage: ${phone.mainFeatures ? phone.mainFeatures.storage :'no storage found'} </p>
  `
 
 }
